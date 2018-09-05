e66a6c12099a37aad1381e04bfdbf1a1410ee923
Ivanova Anna <annivajo@gmail.com>
2018-09-05 15:31:06 +0300

    Add enum

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -9,6 +9,21 @@ public class Recipe {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer servings;
+    private String source;
+    private String url;
+    private String directions;
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+    @Lob
+    private Byte[] image;
+    @OneToOne(cascade = CascadeType.ALL)
+    private Notes notes;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     public Long getId() {
         return id;
@@ -18,24 +33,21 @@ public class Recipe {
         this.id = id;
     }
 
-    private String description;
-    private Integer prepTime;
-    private Integer cookTime;
-    private Integer servings;
-    private String source;
-    private String url;
-    private String directions;
-    //todo add
-    //private Difficulty difficulty;
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
 
-    @Lob
-    private Byte[] image;
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
 
-    @OneToOne(cascade = CascadeType.ALL)
-    private Notes notes;
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
 
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
-    private Set<Ingredient> ingredients;
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
 
     public String getDescription() {
         return description;

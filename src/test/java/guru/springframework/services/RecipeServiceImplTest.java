6f1814df40ad111c138baf5ceeaba7b16e9a49cf
Ivanova Anna <annivajo@gmail.com>
2018-09-10 19:00:10 +0300

    junit init commit

---
+++ src/test/java/guru/springframework/services/RecipeServiceImplTest.java
@@ -0,0 +1,12 @@
+package guru.springframework.services;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RecipeServiceImplTest {
+
+    @Test
+    public void getRecipes() {
+    }
+}

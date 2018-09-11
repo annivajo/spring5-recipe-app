6e9ae7f7159e74956e0429dc5c8caf2f3e16f941
Ivanova Anna <annivajo@gmail.com>
2018-09-11 16:16:18 +0300

    Add test for index controller

--- src/test/java/guru/springframework/services/RecipeServiceImplTest.java
+++ src/test/java/guru/springframework/services/RecipeServiceImplTest.java
@@ -1,12 +1,46 @@
 package guru.springframework.services;
 
+import ch.qos.logback.core.encoder.EchoEncoder;
+import guru.springframework.domain.Recipe;
+import guru.springframework.repositories.RecipeRepository;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class RecipeServiceImplTest {
 
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception{
+        MockitoAnnotations.initMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
     @Test
-    public void getRecipes() {
+    public void getRecipes() throws Exception{
+
+        Recipe recipe = new Recipe();
+        HashSet recipesData = new HashSet();
+        recipesData.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipesData);
+        Set<Recipe> recipeSet = recipeService.getRecipes();
+
+        assertEquals(recipeSet.size(),1);
+        verify(recipeRepository,  times(3)).findAll();
+
     }
 }

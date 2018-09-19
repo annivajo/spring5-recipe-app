34d4e5bd3b1e3c8e77564d9f13f027cbd768d5d9
Alexandr <arlert@mail.ru>
2018-09-19 13:10:17 +0300

    - Modify controller to delete a recipe  - Add missing integration test for update and create  - Tests for delete

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -17,6 +17,7 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
+    @GetMapping
     @RequestMapping({"recipe/show/{id}"})
     public String showById(@PathVariable String id, Model model){
 
@@ -24,6 +25,7 @@ public class RecipeController {
         return "recipe/show";
     }
 
+    @GetMapping
     @RequestMapping({"recipe/new"})
     public String newRecipe(Model model){
 
@@ -39,10 +41,22 @@ public class RecipeController {
         return "redirect:/recipe/show/"+recipeCommand.getId();
     }
 
+    @GetMapping
     @RequestMapping("recipe/{id}/update")
     public String updateRecipe(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
         return  "recipe/recipeform";
     }
+
+    @GetMapping
+    @RequestMapping("recipe/{id}/delete")
+    public String deleteById(@PathVariable String id){
+        log.debug("Deleting id: "+id);
+
+        recipeService.deleteById(Long.valueOf(id));
+        return "redirect:/";
+    }
+
+
 
 }

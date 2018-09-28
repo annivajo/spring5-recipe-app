9eb2f5809d1b33e9ce4cbb0f2403948d29e7f362
Ivanova Anna <annivajo@gmail.com>
2018-09-28 18:38:21 +0300

    upload image init commit

---
+++ src/test/java/guru/springframework/controllers/ImageControllerTest.java
@@ -0,0 +1,52 @@
+package guru.springframework.controllers;
+
+import guru.springframework.services.ImageService;
+import guru.springframework.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class ImageControllerTest {
+    ImageController controller;
+
+    @Mock
+    ImageService imageService;
+
+    @Mock
+    RecipeService recipeService;
+
+    MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        controller = new ImageController(imageService, recipeService);
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    public void handleImagePost() throws Exception {
+        MockMultipartFile multipartFile = new MockMultipartFile("imagefile", "testing.txt", "text/plain",
+                "Spring Framework Guru".getBytes());
+
+        mockMvc.perform(multipart("/recipe/1/image").file(multipartFile))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().string("Location", "/recipe/1/show"));
+
+        verify(imageService, times(1)).saveImageFile(anyLong(),any());
+    }
+
+}
